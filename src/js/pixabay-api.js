676efe044888514807c65a1efc526b2cb56b3e12
KatalinLengyel <158131186+KatalinLengyel@users.noreply.github.com>
2024-07-20 00:47:14 +0300

    Tema11

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,12 +1,30 @@
-import axios from "axios";
+import axios from 'axios';
+import Notiflix from 'notiflix';
 
-const API_KEY = "";
-const BASE_URL = "";
+const API_KEY = '45011386-47ab3bf2a06b79217a4a93c25';
+const URL = 'https://pixabay.com/api/';
+const HITSPERPAGE = 40;
 
-export const fetchImages = async (query, page = 1) => {
-    const response = await axios.get(`${BASE_URL}`, {
-        params: {
-            key: API_KEY}
-    })
-    
-}
+const fetchImages = async (query, page = 1) => {
+  try {
+    const response = await axios.get(`${URL}`, {
+      params: {
+        key: API_KEY,
+        q: query,
+        image_type: 'photo',
+        orientation: 'horizontal',
+        safesearch: true,
+        page,
+        per_page: `${HITSPERPAGE}`,
+      },
+    });
+    return response.data;
+  } catch (error) {
+    console.log('Error:', error);
+    Notiflix.Notify.failure(
+      'Oops! Something went wrong! Try reloading the page!'
+    );
+  }
+};
+
+export { fetchImages, HITSPERPAGE };
